0dd1974524410d9d280c42e4fc7b4225eb4a0b19
ianandpandey <ianandpandey99@gmail.com>
2022-11-26 19:01:53 +0530

    redux

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,13 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div style={{ display: "flex", alignItems: "space-between" }}>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
       <span className="logo">Redux App Store</span>
       <div>
         <Link className="navLink" to="/">
--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -8,12 +8,24 @@ const Products = () => {
       const res = await fetch("https://fakestoreapi.com/products");
       const data = await res.json();
       console.log(data);
+      setProduct(data);
     };
 
     fetchProducts();
   }, []);
 
-  return <div>Products</div>;
+  return (
+    <div className="productsWrapper">
+      {product.map((product) => (
+        <div className="card" key={product.id}>
+          <img src={product.image} alt="images" />
+          <h4>{product.title}</h4>
+          <h5>{product.price}</h5>
+          <button className="btn">Add to Cart</button>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Products;
